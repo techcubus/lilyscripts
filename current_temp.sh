eea0a55b3fef46bbd99747dca3756b8f7968ad6d
Ruri Hoshino <rhoshino@catalyst>
2024-01-13 06:04:02 -0600

    Updated to use weather.gov API

--- current_temp.sh
+++ current_temp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-/usr/bin/curl -s "http://api.wunderground.com/weatherstation/WXCurrentObXML.asp?ID=KSTL" \
-    | /usr/bin/grep temperature_string \
-    | /usr/bin/sed -e 's/.*<temperature_string>\(.*\)<\/temperature_string>/\1/'
+NOAA_STATION=KSTL
+SERVICE_URL="https://api.weather.gov/stations/${NOAA_STATION}/observations/latest"
+
+TEMP_C=$(/usr/bin/curl -s ${SERVICE_URL} \
+    | /usr/bin/jq '.properties.temperature.value' \
+    )
+
+TEMP_F=$(echo "1.8 * ${TEMP_C} + 32" | /usr/bin/bc)
+
+echo "$TEMP_F °F"
